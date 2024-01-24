d3a282f2148d5b636419c55627a15905d0104739
Pavel Golubinec <7respawnables7@gmail.com>
2024-01-24 18:19:10 +0300

    edit func and add new func

--- str_func.py
+++ str_func.py
@@ -3,3 +3,10 @@ def upper(x):
   return ''.join([i for i in x if i.isupper()])
 
 print (upper('ABcc'))
+
+def upper_first(text):
+  """функция делает заглавными первые буквы слов"""
+  text = ' '.join(word[0].upper() + word[1:] for word in text.split())
+  return text
+
+print(upper_first('делаем первые буквы заглавными в python'))
